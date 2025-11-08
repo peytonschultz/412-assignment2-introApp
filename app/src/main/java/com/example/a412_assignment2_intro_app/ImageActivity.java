37069b48c829db576cf3a6a81b66d884a5a157ed
Peyton Schultz <peyton.l.schultz@gmail.com>
2025-11-08 09:06:09 -0600

    added ImageActivity with a layout file to be used for further development.

---
+++ app/src/main/java/com/example/a412_assignment2_intro_app/ImageActivity.java
@@ -0,0 +1,37 @@
+package com.example.a412_assignment2_intro_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_image);
+
+        Button backButton = findViewById(R.id.btn_back);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent explicitIntent = new Intent( ImageActivity.this, MainActivity.class);
+                startActivity(explicitIntent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
