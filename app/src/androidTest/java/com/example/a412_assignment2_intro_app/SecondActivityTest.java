a4ce81e7fe5808dd9eed5b3da96bb7235c791c3e
Peyton Schultz <peyton.l.schultz@gmail.com>
2025-12-02 19:14:58 -0600

    erased credit jdocs to github linked to assignment as it was not used. added credit to class header

--- app/src/androidTest/java/com/example/a412_assignment2_intro_app/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/a412_assignment2_intro_app/SecondActivityTest.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2015, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.a412_assignment2_intro_app;
 
 import org.junit.Before;
@@ -49,6 +33,7 @@ import java.util.List;
 
 /**
  * Basic sample for unbundled UiAutomator.
+ * Some method names and techniques sourced from https://github.com/android/testing-samples/blob/main/ui/uiautomator/BasicSample/app/src/androidTest/java/com/example/android/testing/uiautomator/BasicSample/ChangeTextBehaviorTest.java
  */
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
